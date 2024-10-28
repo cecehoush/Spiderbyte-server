5d2ce8b6d63566d590aa3a1a3162a260c5ff6f5a
Benjamin Fast <89418456+bfast3@users.noreply.github.com>
2024-10-27 18:20:31 -0600

    added users stuff in

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -17,3 +17,48 @@ export async function createUser(req, res) {
     return res.status(500).json({ error: err });
   }
 }
+
+// Function to get a single user by firebase ID
+export async function getUserByFirebaseId(req, res) {
+  try {
+    // MongoDB query to find a user by firebase ID
+    const user = await User.findOne({ firebase_id: req.params.firebaseId });
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500);
+  }
+}
+
+// Function to update a user by firebase ID
+export async function updateUserByFirebaseId(req, res) {
+  try {
+    // MongoDB query to update a user by firebase ID
+    const user = await User.findOne({ firebase_id: req.params.firebaseId });
+    if (!user) {
+      return res.status(404);
+    }
+    Object.assign(user, req.body);
+    await user.save();
+    return res.status(200).json(user);
+  }
+  catch (err) {
+    return res.status(500);
+  }
+}
+
+// Function to delete a user by firebase ID
+export async function deleteUserByFirebaseId(req, res) {
+  try {
+    // MongoDB query to delete a user by firebase ID
+    const user = await User.findOne({ firebase_id: req.params.firebaseId });
+    if (!user) {
+      return res.status(404);
+    }
+    await user.remove();
+    return res.status(200).json({ message: 'User deleted' });
+  }
+  catch (err) {
+    return res.status(500);
+  }
+}
+
